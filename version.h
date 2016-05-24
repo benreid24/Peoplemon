708d15bf3438422f9d1597b33051f5bf04de9e1d
Ben <benreid@buffalo.edu>
2016-05-24 13:26:22 -0400

    Fixed credits and problem with map unique ids for items in hometowndemo.map

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 9;
 	static const long MINOR  = 9;
-	static const long BUILD  = 332;
-	static const long REVISION  = 1787;
+	static const long BUILD  = 333;
+	static const long REVISION  = 1796;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 3475;
-	#define RC_FILEVERSION 9,9,332,1787
-	#define RC_FILEVERSION_STRING "9, 9, 332, 1787\0"
-	static const char FULLVERSION_STRING [] = "9.9.332.1787";
+	static const long BUILDS_COUNT  = 3479;
+	#define RC_FILEVERSION 9,9,333,1796
+	#define RC_FILEVERSION_STRING "9, 9, 333, 1796\0"
+	static const char FULLVERSION_STRING [] = "9.9.333.1796";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 21;
+	static const long BUILD_HISTORY  = 22;
 	
 
 }
