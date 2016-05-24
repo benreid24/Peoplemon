bdea2ea3ec5e6ad0233a849ae610560cd74c8eaa
Ben <benreid@buffalo.edu>
2016-05-24 13:45:27 -0400

    Updated to next major version to reflect demo release

--- version.h
+++ version.h
@@ -14,16 +14,16 @@ namespace AutoVersion{
 	static const char STATUS_SHORT[] =  "a";
 	
 	//Standard Version Type
-	static const long MAJOR  = 9;
-	static const long MINOR  = 9;
-	static const long BUILD  = 334;
-	static const long REVISION  = 1806;
+	static const long MAJOR  = 10;
+	static const long MINOR  = 0;
+	static const long BUILD  = 0;
+	static const long REVISION  = 1;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 3483;
-	#define RC_FILEVERSION 9,9,334,1806
-	#define RC_FILEVERSION_STRING "9, 9, 334, 1806\0"
-	static const char FULLVERSION_STRING [] = "9.9.334.1806";
+	static const long BUILDS_COUNT  = 3484;
+	#define RC_FILEVERSION 10,0,0,1
+	#define RC_FILEVERSION_STRING "10, 0, 0, 1\0"
+	static const char FULLVERSION_STRING [] = "10.0.0.1";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 23;
