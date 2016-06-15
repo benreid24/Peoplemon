292a900e0580556ac16d924e6e2d3487ace3629b
Ben <benreid@buffalo.edu>
2016-06-15 13:39:38 -0400

    Deleted all old maps/tilesets and copied ones for actual game. Copied in animations, tiles and spritesheets for game as well

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "05";
+	static const char DATE[] = "15";
 	static const char MONTH[] = "06";
 	static const char YEAR[] = "2016";
 	static const char UBUNTU_VERSION_STYLE[] =  "16.06";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 10;
 	static const long MINOR  = 4;
-	static const long BUILD  = 48;
-	static const long REVISION  = 279;
+	static const long BUILD  = 49;
+	static const long REVISION  = 282;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 3550;
-	#define RC_FILEVERSION 10,4,48,279
-	#define RC_FILEVERSION_STRING "10, 4, 48, 279\0"
-	static const char FULLVERSION_STRING [] = "10.4.48.279";
+	#define RC_FILEVERSION 10,4,49,282
+	#define RC_FILEVERSION_STRING "10, 4, 49, 282\0"
+	static const char FULLVERSION_STRING [] = "10.4.49.282";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 1;
+	static const long BUILD_HISTORY  = 2;
 	
 
 }
