0afddc063b5a80c1c2f486af806673b39804be1b
Ben <benreid@buffalo.edu>
2016-06-28 12:18:53 -0400

    Incremented version

--- version.h
+++ version.h
@@ -14,19 +14,19 @@ namespace AutoVersion{
 	static const char STATUS_SHORT[] =  "a";
 	
 	//Standard Version Type
-	static const long MAJOR  = 10;
-	static const long MINOR  = 6;
-	static const long BUILD  = 118;
-	static const long REVISION  = 662;
+	static const long MAJOR  = 11;
+	static const long MINOR  = 1;
+	static const long BUILD  = 1;
+	static const long REVISION  = 1;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 3694;
-	#define RC_FILEVERSION 10,6,118,662
-	#define RC_FILEVERSION_STRING "10, 6, 118, 662\0"
-	static const char FULLVERSION_STRING [] = "10.6.118.662";
+	static const long BUILDS_COUNT  = 3708;
+	#define RC_FILEVERSION 11,1,1,1
+	#define RC_FILEVERSION_STRING "11, 1, 1, 1\0"
+	static const char FULLVERSION_STRING [] = "11.1.1.1";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 1;
+	static const long BUILD_HISTORY  = 4;
 	
 
 }
