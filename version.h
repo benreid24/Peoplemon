a0d90dffb63cfff6bd2742d1f71311356b5c857b
Ben <reidben24@gmail.com>
2016-07-26 10:24:09 -0400

    Replaced graphics for ip enter object, somehow the old incorrect ones were in there

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "24";
+	static const char DATE[] = "26";
 	static const char MONTH[] = "07";
 	static const char YEAR[] = "2016";
 	static const char UBUNTU_VERSION_STYLE[] =  "16.07";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 11;
 	static const long MINOR  = 9;
-	static const long BUILD  = 294;
-	static const long REVISION  = 1534;
+	static const long BUILD  = 296;
+	static const long REVISION  = 1545;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 4264;
-	#define RC_FILEVERSION 11,9,294,1534
-	#define RC_FILEVERSION_STRING "11, 9, 294, 1534\0"
-	static const char FULLVERSION_STRING [] = "11.9.294.1534";
+	static const long BUILDS_COUNT  = 4270;
+	#define RC_FILEVERSION 11,9,296,1545
+	#define RC_FILEVERSION_STRING "11, 9, 296, 1545\0"
+	static const char FULLVERSION_STRING [] = "11.9.296.1545";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 17;
+	static const long BUILD_HISTORY  = 19;
 	
 
 }
