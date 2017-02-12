26f142a61e1554b7a2dcab9b3fdcba78cc795cd4
Ben <reidben24@gmail.com>
2017-02-12 18:23:01 -0500

    Test commit

--- version.h
+++ version.h
@@ -2,32 +2,32 @@
 #define VERSION_H
 
 namespace AutoVersion{
-	
+
 	//Date Version Types
 	static const char DATE[] = "23";
 	static const char MONTH[] = "01";
 	static const char YEAR[] = "2017";
 	static const char UBUNTU_VERSION_STYLE[] =  "17.01";
-	
+
 	//Software Status
 	static const char STATUS[] =  "Alpha";
 	static const char STATUS_SHORT[] =  "a";
-	
+
 	//Standard Version Type
 	static const long MAJOR  = 12;
 	static const long MINOR  = 0;
 	static const long BUILD  = 364;
 	static const long REVISION  = 1966;
-	
+
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 4419;
 	#define RC_FILEVERSION 12,0,364,1966
 	#define RC_FILEVERSION_STRING "12, 0, 364, 1966\0"
 	static const char FULLVERSION_STRING [] = "12.0.364.1966";
-	
+
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 17;
-	
+
 
 }
-#endif //VERSION_H
+#endif
